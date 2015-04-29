293e63ef2eec9cac5e0d505d51ba30162d22635c
bmeism <news.manuelsworld@gmail.com>
2015-04-29 15:51:43 +0200

    added pos

--- core/src/ninja/chuun/Spike.java
+++ core/src/ninja/chuun/Spike.java
@@ -1,10 +1,14 @@
 package ninja.chuun;
 
+import com.badlogic.gdx.math.Vector2;
+
 /**
  * Created by bmeism on 17.04.2015.
  */
 public class Spike {
-    public Spike(Map map, int x, int y) {
+    public Vector2 pos;
 
+    public Spike(Map map, int x, int y) {
+        pos = new Vector2(x,y);
     }
 }
