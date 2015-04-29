42f6976d946b7872834d35b5d4ff5dfc1cfd27fc
speedwhy <lukas.walker@bluewin.ch>
2015-04-29 15:32:28 +0200

    added class trampolin

--- core/src/ninja/chuun/Trampolin.java
+++ core/src/ninja/chuun/Trampolin.java
@@ -2,11 +2,10 @@ package ninja.chuun;
 
 import com.badlogic.gdx.math.Rectangle;
 
-
-public class EndDoor {
+public class Trampolin {
     public Rectangle bounds = new Rectangle();
 
-    public EndDoor(Map map, int x, int y) {
+    public Trampolin(Map map, int x, int y) {
         this.bounds.x = x;
         this.bounds.y = y;
         this.bounds.width = this.bounds.height = 1;
